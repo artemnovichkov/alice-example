265d2d534c5c9995974a0c1bd152712ca0e0dc56
Artem Novichkov <artem.novichkov@rosberry.com>
2020-06-14 19:23:19 +0600

    Update parameters

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,8 +8,9 @@ public func routes(_ router: Router) throws {
     }
     
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello, \(name)!"
     }
 
     // Example of configuring a controller
